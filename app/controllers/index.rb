122b9b4444029c9e3d7d0d3eadfee6b1f1c34cd3
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-12 20:37:47 -0500

    added models and migrations

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,25 @@
+
 get '/' do
-  # Look in app/views/index.erb
   erb :index
+end
+
+get '/winner' do
+
+  erb :winner
+end
+
+
+post '/race' do
+  @player_1 = Player.find_or_create_by_name(params[:player1])
+  @player_2 = Player.find_or_create_by_name(params[:player2])
+
+  @game = Game.create()
+  @game.rounds << @player_1
+
+  erb :race
+end
+
+post '/results' do
+
+  redirect to '/winner'
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,53 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+// $(document).ready(function(){
+//   var player1_location = 1; 
+//   var player2_location = 1;
+//   // var startTime = new Date(); 
+//   $("body").keyup(function(e) {
+//     if (e.which === 81){ 
+//       $('#player1_strip > td:nth-child('+player1_location+')').removeClass("active");
+//       player1_location = player1_location + 1;
+//       $('#player1_strip > td:nth-child('+player1_location+')').addClass("active");
+//       if (player1_location === 20) {
+//         // var endTime = new Date();
+//         // var race_time = endTime.getTime() - startTime.getTime()
+//         // $.post('/results', {winner: "winnerID", time: "time"} );
+//       };
+//     };
+//     else if (e.keyCode === 80){
+//       $('#player2_strip > td:nth-child('+player2_location+')').removeClass("active");
+//       player2_location = player2_location + 1;
+//       $('#player2_strip > td:nth-child('+player2_location+')').addClass("active");
+//       if (player2_location === 20) {
+//         // var endTime = new Date();
+//         // var race_time = endTime.getTime() - startTime.getTime()
+//         // $.post('/results', {winner: "winnerID", time: "time"} );
+//       };
+//     };
+//   });
+// });
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+$(document).ready(function(){
+  var player1_location = 1;  
+  var player2_location = 1;   
+  $("body").keyup(function(e) {
+    if (e.which==81){ 
+      $('#player1_strip > td:nth-child('+player1_location+')').removeClass("active");
+      player1_location = player1_location + 1;
+      $('#player1_strip > td:nth-child('+player1_location+')').addClass("active");
+      if (player1_location === 20) {
+        var winnerId = $('#player1_strip').attr('data-player');
+        $.post('/results', {winner: winnerId} );
+        // alert("Player 1 is the winner!")
+      };
+    }
+    else if (e.keyCode==80){
+      $('#player2_strip > td:nth-child('+player2_location+')').removeClass("active");
+      player2_location = player2_location + 1;
+      $('#player2_strip > td:nth-child('+player2_location+')').addClass("active");
+      if (player2_location === 20) {
+        $.post('/results', {winner: "winnerID", time: "time"} );
+        // alert("Player 2 is the winner!")
+      }
+    };
+  });
 });
